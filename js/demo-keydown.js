72b5d46fa174c4ee08e117b1b537bc79d778b55e
Penghua <kids5346@gmail.com>
2020-03-11 21:31:34 +0800

    [refactor]: method name

--- js/demo-keydown.js
+++ js/demo-keydown.js
@@ -1,6 +1,6 @@
-window.addEventListener('keydown', addBgColor);
+window.addEventListener('keydown', changeBgColor);
 
-function addBgColor(e) {
+function changeBgColor(e) {
   const box = document.querySelector('.box');
   if(e.keyCode != 13) return;
   box.classList.add('active');
